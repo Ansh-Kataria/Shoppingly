de083b334debb0d5413ad71306b3e1906f398c2d
Ansh Kataria <anshkataria28@gmail.com>
2023-02-20 14:55:36 +0530

    blogs created

---
+++ src/components/BlogCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const BlogCard = () => {
+  return (
+    <div className="col-3">
+      <div className="blog-card">
+        <div className="card-image">
+          <img src="images/blog-1.jpg" className="img-fluid" alt="blog" />
+        </div>
+        <div className="blog-content">
+          <p className="date">20 Feb, 2023</p>
+          <h5 className="title">A beautiful sunday morning renaissance</h5>
+          <p className="desc">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. A
+            architecto illum atque
+          </p>
+          <Link to="/" className="button">
+            Read More
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
