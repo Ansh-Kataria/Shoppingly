d444a0e5ae3cabaa8035b5937e7f9ac3bce02191
Ansh Kataria <anshkataria28@gmail.com>
2023-02-21 19:10:32 +0530

    Special product started

---
+++ src/components/SpecialProduct.js
@@ -0,0 +1,45 @@
+import React from "react";
+import ReactStars from "react-rating-stars-component";
+import { Link } from "react-router-dom";
+
+const SpecialProduct = () => {
+  return (
+    <div className="col-4">
+      <div className="special-product-card">
+        <div className="d-flex justify-content-between">
+          <div>
+            <img src="images/watch.jpg" alt="watch" />
+          </div>
+          <div className="special-product-content">
+            <h5 className="brand">Havels</h5>
+            <h6 className="title">
+              Samsung Galaxy Note10+ Mobile Phone; Sim...
+            </h6>
+            <ReactStars
+              count={5}
+              size={24}
+              value={4}
+              edit={false}
+              activeColor="#ffd700"
+            />
+            <p className="price">
+              <span className="red-p">$100</span> &nbsp; <strike>$200</strike>
+            </p>
+            <div className="discount-till d-flex align-items-center gap-10">
+              <p className="mb-0">
+                <b>5 </b>days
+              </p>
+              <div className="d-flex gap-10 align-items-center">
+                <span className="badge rounded-circle p-3 bg-danger">1</span>:
+                <span className="badge rounded-circle p-3 bg-danger">1</span>:
+                <span className="badge rounded-circle p-3 bg-danger">1</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SpecialProduct;
