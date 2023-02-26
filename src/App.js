2a25fcfaa487870402f6363e62131f665da8890f
Ansh Kataria <anshkataria28@gmail.com>
2023-02-26 21:46:09 +0530

    our store page created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import Layout from "./components/Layout";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import OurStore from "./pages/OurStore";
 
 const App = () => {
   return (
@@ -16,6 +17,7 @@ const App = () => {
             <Route index element={<Home />} />
             <Route path="about" element={<About />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="store" element={<OurStore />} />
           </Route>
         </Routes>
       </BrowserRouter>
