684eb3dfd01d850fa5df2b08041c020332cd18be
Ansh Kataria <anshkataria28@gmail.com>
2023-03-06 20:27:48 +0530

    updates

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -9,55 +9,57 @@ const SignUp = () => {
       <Meta title={"Sign Up"} />
       <BreadCrumb title="Sign Up" />
       <div className="login-wrapper py-5 home-wrapper-2">
-        <div className="row">
-          <div className="col-12">
-            <div className="auth-card">
-              <h3 className="text-center mb-3">Sign Up</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <div>
-                  <input
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    className="form-control"
-                  />
-                </div>
-
-                <div>
-                  <input
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                    className="form-control"
-                  />
-                </div>
-
-                <div>
-                  <input
-                    type="tel"
-                    name="mobile"
-                    placeholder="Mobile Number"
-                    className="form-control"
-                  />
-                </div>
-
-                <div>
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    className="form-control"
-                  />
-                </div>
-
-                <div>
-                  <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
-                    <Link to="/signup" className="button signup">
-                      SignUp
-                    </Link>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="auth-card">
+                <h3 className="text-center mb-3">Sign Up</h3>
+                <form action="" className="d-flex flex-column gap-15">
+                  <div>
+                    <input
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                      className="form-control"
+                    />
                   </div>
-                </div>
-              </form>
+
+                  <div>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div>
+                    <input
+                      type="tel"
+                      name="mobile"
+                      placeholder="Mobile Number"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      className="form-control"
+                    />
+                  </div>
+
+                  <div>
+                    <div className="mt-3 d-flex justify-content-center gap-15 align-items-center">
+                      <Link to="/login" className="button signup">
+                        SignUp
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
