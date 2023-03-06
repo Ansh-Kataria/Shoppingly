2f1b26f52d92cfccfdfb943f14b6615d3aa41eaa
Ansh Kataria <anshkataria28@gmail.com>
2023-03-07 00:57:03 +0530

    footer links updated

--- src/pages/RefundPolicy.js
+++ src/pages/RefundPolicy.js
@@ -7,6 +7,15 @@ const RefundPolicy = () => {
     <>
       <Meta title={"Refund Policy"} />
       <BreadCrumb title="Refund Policy" />
+      <section className="policy-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="policy"></div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
